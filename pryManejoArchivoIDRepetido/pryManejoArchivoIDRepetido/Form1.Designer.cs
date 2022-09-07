57d8858fb8a0175ddcedcbc6764ad342d2ea0b32
profecasale <112002132+profecasale@users.noreply.github.com>
2022-09-07 14:40:47 -0300

    Add files via upload
    20220907

---
+++ pryManejoArchivoIDRepetido/pryManejoArchivoIDRepetido/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace pryManejoArchivoIDRepetido
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(97, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigo.Location = new System.Drawing.Point(212, 12);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 38);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(212, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(208, 38);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.Location = new System.Drawing.Point(274, 126);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(146, 54);
+            this.btnRegistrar.TabIndex = 4;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(103, 225);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(317, 159);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 453);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registrar Productos (Golosinas)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ pryManejoArchivoIDRepetido/pryManejoArchivoIDRepetido/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace pryManejoArchivoIDRepetido
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            bool puedograbar = true;
+            //el código ya existe?
+            StreamReader objLeerArchivo = new StreamReader("./bdProducto.txt");
+
+            //que sea distinto al final, entra y repite el còdigo
+            while (!objLeerArchivo.EndOfStream)
+            {
+                string[] vecLeido = objLeerArchivo.ReadLine().Split(Convert.ToChar(","));
+
+                if (vecLeido[0] == txtCodigo.Text)
+                {
+                    MessageBox.Show("Còdigo repetido, vuelva a ingresar");
+                    txtCodigo.Focus();
+                    txtCodigo.SelectAll();
+                    puedograbar = false;
+                    //bandera donde diga que el codigo esta repetido
+                }             
+            }
+            objLeerArchivo.Close();
+
+
+            //preguntar
+            if (puedograbar == true)
+            {
+
+                //abrir archivo, indicar que sea para editar
+                StreamWriter objEscribir = new StreamWriter("./bdProducto.txt", true);
+
+                //objEscribir.Write(txtCodigo.Text);
+                //objEscribir.Write(txtNombre.Text);
+
+                //concatenar y agrego una coma
+                objEscribir.WriteLine(txtCodigo.Text + "," + txtNombre.Text);
+
+                objEscribir.Close();
+                MessageBox.Show("Registrado");
+
+                txtCodigo.Text = "";
+                txtNombre.Text = "";
+                txtCodigo.Focus();
+
+                llamarAListar();
+
+            }
+
+          
+       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("./bdProducto.txt"))
+            {
+                //MessageBox.Show("Existe!");
+            }
+            else
+            {
+                File.Create("./bdProducto.txt");
+                //MessageBox.Show("no lo encontramos maestro,,,");
+            }
+
+            //File.Exists("./bdProducto.txt");
+            //
+            llamarAListar();
+        }
+
+
+        private void llamarAListar()
+        {
+            listBox1.Items.Clear();
+
+            //abro el archivo, lo leo y muestro
+            StreamReader objLeerArchivo = new StreamReader("./bdProducto.txt");
+
+            //que sea distinto al final, entra y repite el còdigo
+            while (!objLeerArchivo.EndOfStream)
+            {
+                //string textoLeido = objLeerArchivo.ReadLine();
+
+                //substring sirve para leer de una cadena de caracteres
+                //desde una posicion inicial hasta la cantidad que indique
+                //listBox1.Items.Add(textoLeido.Substring(0,2));
+
+                //creame un vector en base a lo que lees
+                //split es separar, separame por el caracter "," (còdigo ascii)
+                string[] vecLeido = objLeerArchivo.ReadLine().Split(Convert.ToChar(","));
+                listBox1.Items.Add(vecLeido[0].ToString());
+
+            }
+
+            //cierro el archivo
+            objLeerArchivo.Close();
+        }
+
+    }
+}
